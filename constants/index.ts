a594407b7cab4b50baabc8058aa5a0c28476b218
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-05-10 13:32:58 +0530

    setting navbar and sidebar

---
+++ constants/index.ts
@@ -0,0 +1,33 @@
+export const sidebarLinks = [
+    {
+        label:'Home',
+        imgUrl:'/icons/something.svg',
+        route:'/',
+
+    },
+
+    {
+        label:'Upcoming',
+        imgUrl:'/icons/upcoming.svg',
+        route:'/upcoming',
+
+    },  
+     {
+        label:'Previous',
+        imgUrl:'/icons/previous.svg',
+        route:'/previous',
+
+    },  
+     {
+        label:'Recordings',
+        imgUrl:'/icons/Video.svg',
+        route:'/recordings',
+
+    },
+    {
+        label:'Personal Room',
+        imgUrl:'/icons/app-personal.svg',
+        route:'/personal-room',
+
+    },
+]
