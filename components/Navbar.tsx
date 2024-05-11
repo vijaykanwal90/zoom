1dd6dcb1ceecfebd6f0b2bac52af410b9016def5
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-05-11 19:01:02 +0530

    facing third party cookies blocked issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 const Navbar = () => {
   return (
     <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
@@ -16,6 +17,11 @@ const Navbar = () => {
         <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Zomm</p>
       </Link>
       <div className='flex-between gap-5'>
+      {/* <SignedIn>
+              <UserButton />
+            </SignedIn> */}
+
+            
         <MobileNav />
       </div>
     </nav>
