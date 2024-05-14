b749c7cdf6daf4c5a9edcc76ba808a43224ed5fb
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-05-14 19:42:30 +0530

    scheduling meeting room

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
 import React , {ReactNode} from 'react'
-
+import StreamVideoProvider from '@/providers/StreamClientProvider'
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
     <main>
+      <StreamVideoProvider>
       {children}
+
+      </StreamVideoProvider>
+
     </main>
   )
 }
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,11 +5,48 @@ import HomeCard from "./HomeCard"
 import MeetingModal from "./MeetingModal"
 import { useState } from "react"
 import { useRouter } from "next/navigation"
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { useUser } from "@clerk/nextjs"
 const MeetingTypeList = () => {
-    const [meetingState, setMeetingState]= useState<'isScheduleMeeting'| 'isJoiningMeeting'|'isInstantMeeting'| undefined>()
+    const [meetingState, setMeetingState]= useState<'isScheduleMeeting'| 'isJoiningMeeting'|'isInstantMeeting'| undefined>(undefined)
     const router = useRouter()
-    const createMeeting=()=>{
+    const {user} = useUser()
+    const client = useStreamVideoClient()
+    const [values,setValues]= useState({
+      dateTime: new Date(),
+      description:'',
+      link:''
+    })
+    const [callDetails,setCallDetails]= useState<Call>()
+    const createMeeting= async()=>{
+      if(!client || !user){
+        return ;
+      }
+      try {
+        const id = crypto.randomUUID();
+        const call = client.call('default',id);
+        if(!call){
+          throw new Error('failed to create call')
+        }
+        const startsAt= values.dateTime.toISOString()
+        new Date(Date.now()).toISOString();
+        const description = values.description || 'Instant meeting';
 
+        await call.getOrCreate({
+          data:{
+            starts_at:startsAt,
+            custom:{
+              description
+            }
+          }
+        })
+        setCallDetails(call);
+        if(!values.description){
+          router.push(`/meeting${call.id}`)
+        }
+      } catch (error) {
+        console.log(error + " error in meeting type list")
+      }
     }
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -54,9 +91,6 @@ title="Start an Instant Meeting"
 className="text-center"
 buttonText="Start Meeting"
 handleClick= {createMeeting}
-
-
-
 />
     </section>
   )
