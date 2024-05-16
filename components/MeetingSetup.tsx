d928db5ef9dad607129fd5b9a2411b754483ec9b
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-05-16 17:18:21 +0530

    meeting setup

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,8 +1,27 @@
-import { VideoPreview } from '@stream-io/video-react-sdk'
-import React from 'react'
+"use client"
+import { VideoPreview, useCall } from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
 
 const MeetingSetup = () => {
-    const [isMicCamToggledOn,setIsMicCamToo]
+    const [isMicCamToggledOn,setIsMicCamToggledOn]= useState(true)
+    const call= useCall();
+    if(!call){
+        throw new Error('usec all must be used within gstreamcall component')
+    }
+    useEffect(()=>{
+        if(isMicCamToggledOn){
+            call?.camera.disable();
+            call?.microphone.disable();
+
+
+        }
+        else {
+            call?.camera.enable();
+            call?.microphone.enable();
+
+            
+        }
+    },[isMicCamToggledOn, call?.camera, call?.microphone])
   return (
     <div className='flex h-screen w-full flox-col items-center justify-center gap-3 text-white '>
       <h1 className='text-2xl font-bold'></h1>
